b39474da5d756289de7c47e869efcac55b5d2ee4
Connie Leung <cleung334@gmail.com>
2025-05-07 22:21:24 +0800

    feat: complete day 1 and 2 exercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'fundamental-angular';
+  header = signal("Shopping List App");
+  items = signal([
+    {
+      id: 1,
+      label: "10 Apples"
+    },
+    {
+      id: 2,
+      label: "20 Bananas"
+    },
+    {
+      id: 3,
+      label: "5 Oranges"
+    }
+  ]);
 }
